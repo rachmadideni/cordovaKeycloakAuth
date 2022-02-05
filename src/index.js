9502aebc059ad00b8521dc8b7009e79ef585fca8
Denny Rachmadi <rachmadideni@gmail.com>
2022-02-05 11:06:48 +0800

    feat(cordova-setup): render react & update cordova asset path

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const renderReact = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+};
+
+if (window.cordova) {
+  document.addEventListener(
+    "deviceready",
+    () => {
+      renderReact();
+    },
+    false
+  );
+} else {
+  renderReact();
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
